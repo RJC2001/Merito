4eb6abe3491a8d965df85cc1523e8f9142c9e0fc
RJC2001 <Rafal.2001.c@gmail.com>
2025-10-18 18:25:19 +0200

    repair

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-<<<<<<< HEAD
     id("com.google.gms.google-services")
-=======
->>>>>>> origin/main
 }
 
 android {
@@ -49,9 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-<<<<<<< HEAD
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-analytics")
-=======
->>>>>>> origin/main
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-<<<<<<< HEAD
     id("com.google.gms.google-services") version "4.4.4" apply false
-=======
->>>>>>> origin/main
 }
